d7968a2e73fba00c7e29ad8d6575c6544df3b2c7
Vishnu Yelde <vishnuyelde@gmail.com>
2026-01-08 23:54:14 +0530

    Added transactions, addtransaction and budget JSP pages

--- src/main/java/com/pfm/controller/BudgetController.java
+++ src/main/java/com/pfm/controller/BudgetController.java
@@ -1,8 +1,13 @@
 package com.pfm.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BudgetController {
-
+	
+	@GetMapping("/budget")
+	public String addBudget() {
+		return "budget";
+	}
 }
