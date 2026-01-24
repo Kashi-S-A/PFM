2c4baf5609a2df0a13c6686e001442a0798ef5d3
Kashappa Appannavar <129708799+Kashi-S-A@users.noreply.github.com>
2026-01-24 19:13:37 +0530

    Integrate EmailService for welcome email on registration
    Added EmailService to send a welcome email after user registration.

--- src/main/java/com/pfm/serviceimpl/CustomOAuth2UserService.java
+++ src/main/java/com/pfm/serviceimpl/CustomOAuth2UserService.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 import java.util.Map;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -13,14 +14,20 @@ import org.springframework.stereotype.Service;
 
 import com.pfm.entity.User;
 import com.pfm.repo.UserRepo;
+import com.pfm.service.EmailService;
 
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
+	@Autowired
     private final UserRepo userRepo;
+    
+    @Autowired
+	private EmailService emailService;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) {
@@ -46,6 +53,12 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
             user.setPassword(null);
 
             userRepo.save(user);
+            try {
+				emailService.sendWelcomeMail(email, "Welcome to PFM Service", name);
+			} catch (MessagingException e) {
+				e.printStackTrace();
+			}
+
         }
 
         return new DefaultOAuth2User(
